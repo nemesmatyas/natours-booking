dad07dea393ca09cc85e11440e379a2e10f83230
nemesmatyas <nemesmatyas@gmail.com>
2020-04-20 18:39:16 +0200

    Connected app to MongoDB and Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
