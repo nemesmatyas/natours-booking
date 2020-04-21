46740b454399f3a2cf0638c259aac756f55664c4
nemesmatyas <nemesmatyas@gmail.com>
2020-04-21 16:59:11 +0200

    Completed Tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,11 +6,38 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name property'],
     unique: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration property'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize property'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty property'],
+  },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: {
     type: Number,
     required: [true, 'A tour must have a price property'],
   },
+  priceDiscount: { type: Number },
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary property'],
+    trim: true,
+  },
+  description: { type: String, trim: true },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
