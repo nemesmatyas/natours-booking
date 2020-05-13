1eae75e0bce7e4dd52e61a57f4eb8f76149c4a39
nemesmatyas <nemesmatyas@gmail.com>
2020-05-13 20:34:19 +0200

    Added helmetjs for security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -12,6 +13,9 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Helmet - setting security HTTP headers
+app.use(helmet());
+
 // Rate limiter - limit the number of requests coming from an IP address in a given time period to prevent DDOS attacks
 const limiter = rateLimit({
   max: 100,
@@ -21,8 +25,9 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // JSON body parser middleware to have the request body available on the req object when sending a POST request
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
+// Serving media assets as static files - temporary
 app.use(express.static('./public'));
 
 /***************************************************** ROUTES *******************************************************/
