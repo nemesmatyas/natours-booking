50ed1ec5f270f8cb96f2b163ba36d400a1c21393
nemesmatyas <nemesmatyas@gmail.com>
2020-06-08 15:38:04 +0200

    Added calculation of average ratings

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,6 +100,14 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({
+  price: 1,
+  ratingsAverage: -1,
+});
+tourSchema.index({
+  slug: 1,
+});
+
 // eslint-disable-next-line prettier/prettier
 tourSchema.virtual('durationInWeeks').get(function() {
   return this.duration / 7;
